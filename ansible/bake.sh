cb95725f221d8ce528cdce5da36791f3704d0ea9
Anton Cherlyonok <acherlyonok@gmail.com>
2019-02-07 14:58:07 +0300

    Removed make

--- ansible/bake.sh
+++ ansible/bake.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ "$1" == "build" ]; then
-    curl -O https://raw.githubusercontent.com/ansible/ansible/stable-$2/examples/ansible.cfg
-    sed -i -e 's/#retry_files_enabled = False/retry_files_enabled = False/g' ./ansible.cfg
-    sed -i -e 's/#host_key_checking = False/host_key_checking = False/g' ./ansible.cfg
-    docker build -t acherlyonok/ansible:2.6.12 .
-    exit
-fi
+curl -O https://raw.githubusercontent.com/ansible/ansible/stable-2.6/examples/ansible.cfg
+sed -i -e 's/#retry_files_enabled = False/retry_files_enabled = False/g' ./ansible.cfg
+sed -i -e 's/#host_key_checking = False/host_key_checking = False/g' ./ansible.cfg
+docker build -t acherlyonok/ansible:2.6.12 .
